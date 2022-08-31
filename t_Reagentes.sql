8a7c115a01f7c449702a4fbc3f1702d318d86ce4
Aline <aline.revisoes@gmail.com>
2022-08-30 23:01:21 -0300

    alterações no arquivo

--- t_Reagentes.sql
+++ t_Reagentes.sql
@@ -1,54 +1,56 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: mysql_db
--- Tempo de geração: 30-Ago-2022 às 00:29
--- Versão do servidor: 8.0.30
--- versão do PHP: 8.0.22
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: sgpq
+-- ------------------------------------------------------
+-- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `SGPQ`
+-- Table structure for table `t_reagentes`
 --
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `t_Reagentes`
---
-
-CREATE TABLE `t_Reagentes` (
+DROP TABLE IF EXISTS `t_reagentes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `t_reagentes` (
   `id_Reagente` varchar(10) NOT NULL,
   `nr_CAS` varchar(15) NOT NULL,
   `nm_PT` varchar(60) DEFAULT NULL,
   `nm_EN` varchar(60) DEFAULT NULL,
   `gr_Compatibilidade` char(4) NOT NULL,
   `pdf_FISPQ` varchar(100) DEFAULT NULL,
-  `info_Descarte` longtext
+  `info_Descarte` longtext,
+  PRIMARY KEY (`id_Reagente`),
+  UNIQUE KEY `nr_CAS` (`nr_CAS`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Índices para tabelas despejadas
+-- Dumping data for table `t_reagentes`
 --
 
---
--- Índices para tabela `t_Reagentes`
---
-ALTER TABLE `t_Reagentes`
-  ADD PRIMARY KEY (`id_Reagente`),
-  ADD UNIQUE KEY `nr_CAS` (`nr_CAS`);
-COMMIT;
+LOCK TABLES `t_reagentes` WRITE;
+/*!40000 ALTER TABLE `t_reagentes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_reagentes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-30 22:59:26
